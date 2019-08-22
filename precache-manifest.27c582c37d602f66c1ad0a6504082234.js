9fdb5a0191f73780c80637b4cd49216493c5b1a5
David Markešić <d.markesic@altpro.hr>
2019-08-22 04:26:40 +0200

    initial

---
+++ precache-manifest.27c582c37d602f66c1ad0a6504082234.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91980ed639bf4cc9723b1786217b36fd",
+    "url": "%20//slusko88.github.io/index.html"
+  },
+  {
+    "revision": "f43fb6f2b33ac60110f5",
+    "url": "%20//slusko88.github.io/static/css/main.677323f9.chunk.css"
+  },
+  {
+    "revision": "19ea6adc6c530e2cf243",
+    "url": "%20//slusko88.github.io/static/js/2.3211f3c7.chunk.js"
+  },
+  {
+    "revision": "f43fb6f2b33ac60110f5",
+    "url": "%20//slusko88.github.io/static/js/main.298a893a.chunk.js"
+  },
+  {
+    "revision": "ec0cb7a864555f026ad3",
+    "url": "%20//slusko88.github.io/static/js/runtime~main.3888187e.js"
+  },
+  {
+    "revision": "651c597ee3d6a52326c93febc8202d41",
+    "url": "%20//slusko88.github.io/static/media/ap_logo.651c597e.svg"
+  }
+]);
